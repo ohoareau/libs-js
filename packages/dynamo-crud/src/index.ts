a4427dae025613c31f5c707ba2d2eff9f8911d5c
Olivier Hoareau <oha@greenberets.io>
2019-10-14 23:26:13 +0200

    ability to prefix/suffix dynamodb table using env vars

--- packages/dynamo-crud/src/index.ts
+++ packages/dynamo-crud/src/index.ts
@@ -1,14 +1,19 @@
 import dynamoose from 'dynamoose';
 import uuidv4 from 'uuid/v4';
 
+const globalOptions = {
+    prefix: process.env.DYNAMODB_TABLE_PREFIX || undefined,
+    suffix: process.env.DYNAMODB_TABLE_SUFFIX || undefined,
+};
+
 export default ({ type, schema }) => {
-    const Model = dynamoose.model(type, schema, {create: false, update: false, waitForActive: false});
+    const options = {create: false, update: false, waitForActive: false, ...globalOptions};
+    const Model = dynamoose.model(type, schema, options);
     const get = async (id: string): Promise<any> => Model.get(id);
     const find = async (): Promise<any> => ({items: await Model.scan().exec()});
     const create = async (data): Promise<any> => Model.create({...data, id: uuidv4()});
     const update = async (id: string, data: any): Promise<any> => Model.update({ id }, data);
     const remove = async (id: string): Promise<any> => Model.delete({ id });
-
     return {
         get, find, create, update, remove,
     };
