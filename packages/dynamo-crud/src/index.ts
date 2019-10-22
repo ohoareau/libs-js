70d96b1033a75a54db96f3510f23c2eec739f6e5
Olivier Hoareau <oha@greenberets.io>
2019-10-22 17:46:24 +0200

    implement dynamoose find

--- packages/dynamo-crud/src/index.ts
+++ packages/dynamo-crud/src/index.ts
@@ -1,6 +1,15 @@
 import dynamoose from 'dynamoose';
 import uuidv4 from 'uuid/v4';
 
+class DocumentNotFoundError extends Error {
+    public type: String;
+    public id: String;
+    constructor(type, id) {
+        super(`${type} '${id}' does not exist`);
+        this.type = type;
+        this.id = id;
+    }
+}
 const globalOptions = {
     prefix: process.env.DYNAMODB_TABLE_PREFIX || undefined,
     suffix: process.env.DYNAMODB_TABLE_SUFFIX || undefined,
@@ -33,13 +42,50 @@ const hooked = (operation, callback, hooks, service) => async (...args): Promise
     await applyHook(`${operation}Message`, hooks, ctx, createMessageHook);
     return ctx.result;
 };
+const buildQueryDefinitionFromCriteria = (criteria) => {
+    return Object.keys(criteria).reduce((acc, k) => {
+        acc[k] = criteria[k];
+        return acc;
+    }, {});
+};
+
+const runQuery = async (m, {criteria, fields, limit, offset, sort, options}) => {
+    let q = m.query(buildQueryDefinitionFromCriteria(criteria));
+    if (limit) {
+        q = q.limit(limit);
+    }
+    if (fields && fields.length) {
+        q.attributes(fields);
+    }
+    if (offset) {
+        q.startAt(offset);
+    }
+    if (sort) {
+        Object.keys(sort).reduce((qq, k) => {
+            return qq.where(k)[sort[k] === -1 ? 'descending' : 'ascending' ]();
+        }, q);
+    }
+    if (options.consistent) {
+        q.consistent();
+    }
+    if (options.all) {
+        q.all();
+    }
+    return q.exec();
+};
 
 export default ({ type, schema, hooks = {} }) => {
     const service = {};
     const options = {create: false, update: false, waitForActive: false, ...globalOptions};
     const Model = dynamoose.model(type, schema, options);
-    const get = hooked('get', async (id: string): Promise<any> => Model.get(id), hooks, service);
-    const find = hooked('find', async (): Promise<any> => ({items: await Model.scan().exec()}), hooks, service);
+    const get = hooked('get', async (id: string): Promise<any> => {
+        const doc = await Model.get(id);
+        if (!doc) {
+            throw new DocumentNotFoundError(type, id);
+        }
+        return doc;
+    }, hooks, service);
+    const find = hooked('find', async (criteria = {}, fields = [], limit = undefined, offset = 0, sort = undefined, options = {}): Promise<any> => ({ items: await runQuery(Model, {criteria, fields, limit, offset, sort, options}) }), hooks, service);
     const create = hooked('create', async (data): Promise<any> => Model.create({...data, id: uuidv4()}), hooks, service);
     const update = hooked('update', async (id: string, data: any): Promise<any> => Model.update({ id }, data), hooks, service);
     const remove = hooked('remove', async (id: string): Promise<any> => Model.delete({ id }), hooks, service);
