6aa1ac60f5587968b3440cebb592e784b14621ae
Olivier Hoareau <oha@greenberets.io>
2019-11-25 17:36:15 +0100

    fix

--- packages/microservice/src/registers/hooks-cognito.ts
+++ packages/microservice/src/registers/hooks-cognito.ts
@@ -1,5 +1,5 @@
 import p1 from '../plugins/hook/cognito-user-create';
-import p2 from '../plugins/hook/cognito-user-remove';
+import p2 from '../plugins/hook/cognito-user-delete';
 import {register} from "..";
 register('hook', 'cognito-user-create', p1);
-register('hook', 'cognito-user-remove', p2);
+register('hook', 'cognito-user-delete', p2);
