8219e93e376a9c4e128d6224954317e417847738
Olivier Hoareau <oha@greenberets.io>
2019-12-02 10:49:25 +0100

    fix

--- packages/microservice/src/common-handlers/events.ts
+++ packages/microservice/src/common-handlers/events.ts
@@ -1,10 +1,11 @@
 import {Executor, Config, RootConfig, Map, Handler} from "..";
 
-export const factory = (_, c: Config) => async (event: any, context: any) =>
-    (await (<Executor>c.execute)('events', {event, context})).res.result
+export const factory = (_, configs: Config[]) => async (event: any, context: any) =>
+    Promise.all((await Promise.all(configs.map(c => (<Executor>c.execute)('events', {event: {...event}, context})))).map(a => a.res.result))
 ;
+
 export default (c: RootConfig, handlers: Map<Handler>) => {
-    const config = c.types.find(t => !!t.eventSourceBackendExecutor);
-    if (!config) return;
-    handlers.receiveExternalEvents = factory(c, <Config>config);
+    const configs = c.types.filter(t => !!t.eventSourceBackendExecutor);
+    if (0 === configs.length) return;
+    handlers.receiveExternalEvents = factory(c, <Config[]>configs);
 }
