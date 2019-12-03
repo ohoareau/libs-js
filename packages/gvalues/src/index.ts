e75ecdec6e3f0e0cd877c95b3d75597f1d82fba4
Olivier Hoareau <oha@greenberets.io>
2019-12-03 17:09:08 +0100

    fix

--- packages/gvalues/src/index.ts
+++ packages/gvalues/src/index.ts
@@ -1,4 +1,19 @@
-const toVar = v => Array.isArray(v) ? arrayToVar(v) : (('object' === typeof v) ? objectToVar(v) : undefined);
+export const buildRawValue = v => {
+    switch (true) {
+        case !!v.value: return v.value[`${v.value.type}Value`];
+        case !!v.entries: return arrayVarToMap(v.entries);
+        case !!v.values: return v.values.map(vv => buildRawValue(vv));
+        default: return undefined;
+    }
+};
+
+export const marshall = v => Array.isArray(v) ? arrayToVar(v) : (('object' === typeof v) ? objectToVar(v) : undefined);
+export const unmarshall = v => Array.isArray(v) ? arrayVarToMap(v) : buildRawValue(v);
+
+export const arrayVarToMap = v => v.reduce((acc, vv) => {
+    acc[vv.name] = buildRawValue(vv);
+    return acc;
+}, {});
 export const detectType = v => {
     const tof = typeof v;
     switch (true) {
@@ -18,8 +33,8 @@ export const pushValue = (acc, kk, vv) => {
         case 'int':     r.value = {type: 'int', intValue: vv}; break;
         case 'boolean': r.value = {type: 'boolean', booleanValue: vv}; break;
         case 'float':   r.value = {type: 'float', floatValue: vv}; break;
-        case 'array':   r.values = toVar(vv); break;
-        case 'map':     r.entries = toVar(vv); break;
+        case 'array':   r.values = marshall(vv); break;
+        case 'map':     r.entries = marshall(vv); break;
         default:        r.value = {type: 'string', stringValue: undefined}; break;
     }
     return acc;
@@ -27,4 +42,4 @@ export const pushValue = (acc, kk, vv) => {
 export const arrayToVar = v => v.reduce((acc, vv, kk) => pushValue(acc, kk, vv), []);
 export const objectToVar = v => Object.entries(v).reduce((acc, [kk, vv]) => pushValue(acc, kk, vv), []);
 
-export default toVar;
+export default (v, reverse = false) => reverse ? unmarshall(v) : marshall(v);
