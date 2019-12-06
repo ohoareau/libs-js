315dd0899d820e75dc03f43a5786622d5b15352e
Olivier Hoareau <oha@greenberets.io>
2019-12-06 13:44:39 +0100

    fix

--- packages/microservice/src/plugins/eventsource/sqs.ts
+++ packages/microservice/src/plugins/eventsource/sqs.ts
@@ -3,66 +3,66 @@ import {Map, TypedMap, Handler, Config} from "../..";
 
 const sqs = new AWS.SQS();
 
-const processDirectMessage = async (ec: TypedMap, c: Config, context: any, r: Map) => {
-    const receiptHandle = r.receiptHandle;
-    const message = JSON.parse(r.body);
-    const eventType = r.messageAttributes.fullType.stringValue.toLowerCase().replace(/\./g, '_');
-    const splits = r.eventSourceARN.split(':');
-    const queueUrl = sqs.endpoint.href + splits[4] + '/' + splits[5];
+const consumeMessage = async (c, {receiptHandle, attributes, rawMessage, eventType, queueUrl}, context) => {
     const listeners = c.getListenersFor(eventType);
-    if (0 < listeners.length) {
-        const attributes = Object.entries(r.messageAttributes).reduce((acc, [k, m]) => {
+    const result = {status: 'ignored', listeners: listeners.length, clean: async (result, {ids, processed, ignored}) => {
+        if (!ignored[receiptHandle]) ignored[receiptHandle] = 0;
+        if (!processed[receiptHandle]) processed[receiptHandle] = 0;
+        switch (result.status) {
+            case 'processed': processed[receiptHandle]++; break;
+            case 'ignored': ignored[receiptHandle]++; break;
+        }
+        if (ids[receiptHandle]) return;
+        await sqs.deleteMessage({QueueUrl: queueUrl, ReceiptHandle: receiptHandle}).promise();
+        ids[receiptHandle] = true;
+    }};
+    if (!listeners.length) return result;
+    const message = JSON.parse(rawMessage);
+    c.log('external event', 'PROCESSING', eventType, message, attributes, receiptHandle, 'ENCAPSULATED');
+    await Promise.all(listeners.map(async (listener: Handler) => listener(
+        message,
+        {attributes, execute: c.execute, config: c, context, queueUrl, receiptHandle}
+    )));
+    c.log('external event', 'PROCESSED', eventType, receiptHandle, 'ENCAPSULATED');
+    result.status = 'processed';
+    return result;
+};
+
+const processDirectMessage = async (ec: TypedMap, c: Config, context: any, r: Map) => {
+    const splits = r.eventSourceARN.split(':');
+    return consumeMessage(c, {
+        receiptHandle: r.receiptHandle,
+        rawMessage: r.body,
+        attributes: Object.entries(r.messageAttributes).reduce((acc, [k, m]) => {
             acc[k] = (<Map>m).stringValue;
             return acc;
-        }, {});
-        c.log('external event', 'PROCESSING', eventType, message, attributes, receiptHandle, 'DIRECT');
-        await Promise.all(listeners.map(async (listener: Handler) => listener(
-            message,
-            {attributes, execute: c.execute, config: c, context, queueUrl, receiptHandle}
-        )));
-        c.log('external event', 'PROCESSED', eventType, receiptHandle, 'DIRECT');
-    } else {
-        c.log('external event', 'IGNORED', eventType, receiptHandle, 'DIRECT');
-    }
-    sqs.deleteMessage({QueueUrl: queueUrl, ReceiptHandle: receiptHandle});
+        }, {}),
+        eventType: r.messageAttributes.fullType.stringValue.toLowerCase().replace(/\./g, '_'),
+        queueUrl: sqs.endpoint.href + splits[4] + '/' + splits[5],
+    }, context);
 };
 
 const processEncapsulatedMessage = async (ec: TypedMap, c: Config, context: any, r: Map) => {
-    const receiptHandle = r.receiptHandle;
     const body = JSON.parse(r.body);
-    const eventType = body.MessageAttributes.fullType.Value.toLowerCase().replace(/\./g, '_');
     const splits = r.eventSourceARN.split(':');
-    const queueUrl = sqs.endpoint.href + splits[4] + '/' + splits[5];
-    if (c.eventListeners && c.eventListeners[eventType]) {
-        const attributes = Object.entries(body.MessageAttributes).reduce((acc, [k, m]) => {
+    return consumeMessage(c, {
+        receiptHandle: r.receiptHandle,
+        rawMessage: body.Message,
+        attributes: Object.entries(body.MessageAttributes).reduce((acc, [k, m]) => {
             acc[k] = (<Map>m).Value;
             return acc;
-        }, {});
-        const message = JSON.parse(body.Message);
-        c.log('external event', 'PROCESSING', eventType, message, attributes, receiptHandle, 'ENCAPSULATED');
-        await (<Handler>c.eventListeners[eventType])(
-            message,
-            {
-                attributes,
-                execute: c.execute,
-                config: c,
-                context,
-                queueUrl,
-                receiptHandle,
-            }
-        );
-        c.log('external event', 'PROCESSED', eventType, receiptHandle, 'ENCAPSULATED');
-    } else {
-        c.log('external event', 'IGNORED', eventType, receiptHandle, 'ENCAPSULATED');
-    }
-    sqs.deleteMessage({QueueUrl: queueUrl, ReceiptHandle: receiptHandle});
+        }, {}),
+        eventType: body.MessageAttributes.fullType.Value.toLowerCase().replace(/\./g, '_'),
+        queueUrl: sqs.endpoint.href + splits[4] + '/' + splits[5],
+    }, context);
 };
 
-export default (ec: TypedMap, c: Config) => async (event: any, context: any): Promise<any> => {
-    if (!event.Records || !event.Records.length) return;
-    await Promise.all(event.Records.map(async r =>
-        (r.messageAttributes && r.messageAttributes.fullType && r.messageAttributes.fullType.stringValue)
-            ? processDirectMessage(ec, c, context, r)
-            : processEncapsulatedMessage(ec, c, context, r)
-    ));
-}
+export default (ec: TypedMap, c: Config) => async (event: any, context: any): Promise<any[]> =>
+    (!event.Records || !event.Records.length)
+        ? []
+        : Promise.all(event.Records.map(async r =>
+            (r.messageAttributes && r.messageAttributes.fullType && r.messageAttributes.fullType.stringValue)
+                ? processDirectMessage(ec, c, context, r)
+                : processEncapsulatedMessage(ec, c, context, r)
+        ))
+;
