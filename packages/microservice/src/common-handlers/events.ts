efd41828d4b2b85e04a4a116263f6c442b23d7d8
Olivier Hoareau <oha@greenberets.io>
2019-12-06 17:30:29 +0100

    fix

--- packages/microservice/src/common-handlers/events.ts
+++ packages/microservice/src/common-handlers/events.ts
@@ -1,10 +1,11 @@
 import {Executor, Config, RootConfig, Map, Handler} from "..";
 
 export const factory = (c, configs: Config[]) => async (event: any, context: any) => {
-    const types = await Promise.all((await Promise.all(configs.map(async c => (<Executor>c.execute)('events', {
-        event: {...event},
-        context
-    })))).map(a => a.res.result));
+    const types = await Promise.all(
+        (await Promise.all(
+            configs.map(async c => (<Executor>c.execute)('events', {event: {...event}, context}))
+        )).map(a => a.res.result)
+    );
     const data = {ids: {}, processed: {}, ignored: {}};
     await Promise.all(types.map(async results => Promise.all(results.map(async result => result.clean(result, data)))));
     c.log(`external event REPORT received=${Object.keys(data.ids).length}, processed=${Object.keys(data.processed).length}, ignored=${Object.keys(data.ignored).length}`);
--- packages/microservice/src/plugins/config/event.ts
+++ packages/microservice/src/plugins/config/event.ts
@@ -1,11 +1,11 @@
 import {Map, Context, Config} from "../..";
 
 export default (ctx: Context, c: Config): void => {
+    c.getListenersFor = (type: string) => (c.eventListeners || {})[type] || [];
     if (!c.events || ('object' !== typeof c.events) || (0 === Object.keys(c.events).length)) return;
     c.eventListeners = Object.entries(c.events).reduce((acc, [name, items]) => {
         if (!Array.isArray(items)) items = [items];
         acc[name] =  (<any[]>items).map(def => ('function' === typeof def) ? def : (<Map>def).callback);
         return acc;
     }, {});
-    c.getListenersFor = (type: string) => c.eventListeners[type] || [];
 }
