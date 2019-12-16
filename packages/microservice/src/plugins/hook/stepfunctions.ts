5edee1e0148717c418796d0016273ecf16a31752
Olivier Hoareau <oha@greenberets.io>
2019-12-16 11:40:13 +0100

    fix

--- packages/microservice/src/plugins/hook/stepfunctions.ts
+++ packages/microservice/src/plugins/hook/stepfunctions.ts
@@ -7,4 +7,4 @@ export default (cfg, c: Config) => async (action) => sfn.startExecution({
     stateMachine: cfg.stateMachine,
     input: await ((cfg.input && cfg.input.apply) ? cfg.input : async (action) => cfg.input || (cfg.usePayloadData ? action.req.payload.data : (await action.res.result || action.req)))(action) || {},
     namePrefix: `${c.full_type}-${action.req.operation}-`,
-});
+})
