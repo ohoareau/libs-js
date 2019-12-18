c2dfd2e72724dbf78c62875fab91e33674ae5730
Olivier Hoareau <oha@greenberets.io>
2019-12-18 11:24:56 +0100

    fix

--- packages/microservice/src/plugins/hook/stepfunctions.ts
+++ packages/microservice/src/plugins/hook/stepfunctions.ts
@@ -3,8 +3,17 @@ import sfnFactory from '../../factories/sfn';
 
 const sfn = sfnFactory();
 
-export default (cfg, c: Config) => async (action) => sfn.startExecution({
-    stateMachine: cfg.stateMachine,
-    input: await ((cfg.input && cfg.input.apply) ? cfg.input : async (action) => cfg.input || (cfg.usePayloadData ? action.req.payload.data : (await action.res.result || action.req)))(action) || {},
-    namePrefix: `${c.full_type}-${action.req.operation}-`,
-})
+export default (cfg, c: Config) => async (action) => {
+    const input = await ((cfg.input && cfg.input.apply) ? cfg.input : async (action) => cfg.input || (cfg.usePayloadData ? action.req.payload.data : (await action.res.result || action.req)))(action) || {};
+    if (cfg.ensureKeys && Array.isArray(cfg.ensureKeys)) {
+        cfg.ensureKeys.reduce((acc, k) => {
+            acc[k] = acc.hasOwnProperty(k) ? acc[k] : '';
+            return acc;
+        }, input);
+    }
+    return sfn.startExecution({
+        stateMachine: cfg.stateMachine,
+        input,
+        namePrefix: `${c.full_type}-${action.req.operation}-`,
+    });
+}
