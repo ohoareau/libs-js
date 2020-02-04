54ddd500b9f28fb357a400381a250552967fd652
Olivier Hoareau <oha@greenberets.io>
2020-02-04 15:58:17 +0100

    dynamodb scan/query index for microgen

--- packages/microgen/statics/__tests__/services/aws/dynamodb.test.js
+++ packages/microgen/statics/__tests__/services/aws/dynamodb.test.js
@@ -109,3 +109,164 @@ describe('queryIndex', () => {
         });
     });
 });
+describe('query', () => {
+    it('prepare the underlying documentClient query', async () => {
+        const mockQuery = jest.fn();
+        mockQuery.mockReturnValue({promise: () => ({Items: [], Count: 0, ScannedCount: 2})});
+        docClientMock.prototype.query = mockQuery;
+        expect(await dynamodb.query('mytable', {k1: 'v1'})).toEqual({
+            items: [],
+            count: 0,
+            scannedCount: 2,
+        });
+        expect(mockQuery).toHaveBeenCalledWith({
+            ExpressionAttributeNames: {
+                '#K0': 'k1',
+            },
+            ExpressionAttributeValues: {
+                ':v0': 'v1',
+            },
+            TableName: 'mytable',
+            KeyConditionExpression: '#K0 = :v0',
+        });
+    });
+    it('prepare the underlying documentClient query with special operator beginsWith', async () => {
+        const mockQuery = jest.fn();
+        mockQuery.mockReturnValue({promise: () => ({Items: [], Count: 0, ScannedCount: 1})});
+        docClientMock.prototype.query = mockQuery;
+        expect(await dynamodb.query('mytable', {k1: 'hello', k2: {type: 'beginsWith', value: 'xxx.'}})).toEqual({
+            items: [],
+            count: 0,
+            scannedCount: 1,
+        });
+        expect(mockQuery).toHaveBeenCalledWith({
+            ExpressionAttributeNames: {
+                '#K0': 'k1',
+                '#K1': 'k2',
+            },
+            ExpressionAttributeValues: {
+                ':v0': 'hello',
+                ':v1': 'xxx.',
+            },
+            TableName: 'mytable',
+            KeyConditionExpression: '#K0 = :v0 and begins_with(#K1, :v1)',
+        });
+    });
+    it('prepare the underlying documentClient query with special operator eq', async () => {
+        const mockQuery = jest.fn();
+        mockQuery.mockReturnValue({promise: () => ({Items: [], Count: 0, ScannedCount: 1})});
+        docClientMock.prototype.query = mockQuery;
+        expect(await dynamodb.query('mytable', {k1: {type: 'eq', value: 'abcd'}})).toEqual({items: [], count: 0, scannedCount: 1});
+        expect(mockQuery).toHaveBeenCalledWith({
+            ExpressionAttributeNames: {'#K0': 'k1'},
+            ExpressionAttributeValues: {':v0': 'abcd'},
+            TableName: 'mytable',
+            KeyConditionExpression: '#K0 = :v0',
+        });
+    });
+    it('prepare the underlying documentClient query with special operator le', async () => {
+        const mockQuery = jest.fn();
+        mockQuery.mockReturnValue({promise: () => ({Items: [], Count: 0, ScannedCount: 1})});
+        docClientMock.prototype.query = mockQuery;
+        expect(await dynamodb.query('mytable', {k1: {type: 'le', value: 'abcd'}})).toEqual({items: [], count: 0, scannedCount: 1});
+        expect(mockQuery).toHaveBeenCalledWith({
+            ExpressionAttributeNames: {'#K0': 'k1'},
+            ExpressionAttributeValues: {':v0': 'abcd'},
+            TableName: 'mytable',
+            KeyConditionExpression: '#K0 <= :v0',
+        });
+    });
+    it('prepare the underlying documentClient query with special operator lt', async () => {
+        const mockQuery = jest.fn();
+        mockQuery.mockReturnValue({promise: () => ({Items: [], Count: 0, ScannedCount: 1})});
+        docClientMock.prototype.query = mockQuery;
+        expect(await dynamodb.query('mytable', {k1: {type: 'lt', value: 'abcd'}})).toEqual({items: [], count: 0, scannedCount: 1});
+        expect(mockQuery).toHaveBeenCalledWith({
+            ExpressionAttributeNames: {'#K0': 'k1'},
+            ExpressionAttributeValues: {':v0': 'abcd'},
+            TableName: 'mytable',
+            KeyConditionExpression: '#K0 < :v0',
+        });
+    });
+    it('prepare the underlying documentClient query with special operator ge', async () => {
+        const mockQuery = jest.fn();
+        mockQuery.mockReturnValue({promise: () => ({Items: [], Count: 0, ScannedCount: 1})});
+        docClientMock.prototype.query = mockQuery;
+        expect(await dynamodb.query('mytable', {k1: {type: 'ge', value: 'abcd'}})).toEqual({items: [], count: 0, scannedCount: 1});
+        expect(mockQuery).toHaveBeenCalledWith({
+            ExpressionAttributeNames: {'#K0': 'k1'},
+            ExpressionAttributeValues: {':v0': 'abcd'},
+            TableName: 'mytable',
+            KeyConditionExpression: '#K0 >= :v0',
+        });
+    });
+    it('prepare the underlying documentClient query with special operator gt', async () => {
+        const mockQuery = jest.fn();
+        mockQuery.mockReturnValue({promise: () => ({Items: [], Count: 0, ScannedCount: 1})});
+        docClientMock.prototype.query = mockQuery;
+        expect(await dynamodb.query('mytable', {k1: {type: 'gt', value: 'abcd'}})).toEqual({items: [], count: 0, scannedCount: 1});
+        expect(mockQuery).toHaveBeenCalledWith({
+            ExpressionAttributeNames: {'#K0': 'k1'},
+            ExpressionAttributeValues: {':v0': 'abcd'},
+            TableName: 'mytable',
+            KeyConditionExpression: '#K0 > :v0',
+        });
+    });
+    it('prepare the underlying documentClient query with special operator between', async () => {
+        const mockQuery = jest.fn();
+        mockQuery.mockReturnValue({promise: () => ({Items: [], Count: 0, ScannedCount: 1})});
+        docClientMock.prototype.query = mockQuery;
+        expect(await dynamodb.query('mytable', {k1: {type: 'between', lowerBound: 1, upperBound: 10}})).toEqual({items: [], count: 0, scannedCount: 1});
+        expect(mockQuery).toHaveBeenCalledWith({
+            ExpressionAttributeNames: {'#K0': 'k1'},
+            ExpressionAttributeValues: {':v0a': 1, ':v0b': 10},
+            TableName: 'mytable',
+            KeyConditionExpression: '#K0 BETWEEN :v0a AND :v0b',
+        });
+    });
+});
+describe('scan', () => {
+    it('prepare the underlying documentClient query', async () => {
+        const mockScan = jest.fn();
+        mockScan.mockReturnValue({promise: () => ({Items: [], Count: 0, ScannedCount: 2})});
+        docClientMock.prototype.scan = mockScan;
+        expect(await dynamodb.scan('mytable', {k1: 'v1'})).toEqual({
+            items: [],
+            count: 0,
+            scannedCount: 2,
+        });
+        expect(mockScan).toHaveBeenCalledWith({
+            ExpressionAttributeNames: {
+                '#K0': 'k1',
+            },
+            ExpressionAttributeValues: {
+                ':v0': 'v1',
+            },
+            TableName: 'mytable',
+            FilterExpression: '#K0 = :v0',
+        });
+    });
+});
+describe('scanIndex', () => {
+    it('prepare the underlying documentClient query', async () => {
+        const mockScan = jest.fn();
+        mockScan.mockReturnValue({promise: () => ({Items: [], Count: 0, ScannedCount: 2})});
+        docClientMock.prototype.scan = mockScan;
+        expect(await dynamodb.scanIndex('mytable', 'myindex', {k1: 'v1'})).toEqual({
+            items: [],
+            count: 0,
+            scannedCount: 2,
+        });
+        expect(mockScan).toHaveBeenCalledWith({
+            ExpressionAttributeNames: {
+                '#K0': 'k1',
+            },
+            ExpressionAttributeValues: {
+                ':v0': 'v1',
+            },
+            TableName: 'mytable',
+            IndexName: 'myindex',
+            FilterExpression: '#K0 = :v0',
+        });
+    });
+});
--- packages/microgen/statics/services/aws/dynamodb.js
+++ packages/microgen/statics/services/aws/dynamodb.js
@@ -16,27 +16,45 @@ const buildReturns = returns => {
         ReturnValuesOnConditionCheckFailure: returns.valuesOnConditionCheckFailure,
     };
 };
+const buildOperationString = (v, kk, i) => {
+    if ('object' === typeof v) {
+        const {type, ...params} = v;
+        const vv = `:v${i}`;
+        switch (type) {
+            case 'beginsWith': return [[[vv, params.value]], `begins_with(${kk}, ${vv})`];
+            case 'eq': return [[[vv, params.value]], `${kk} = ${vv}`];
+            case 'lt': return [[[vv, params.value]], `${kk} < ${vv}`];
+            case 'le': return [[[vv, params.value]], `${kk} <= ${vv}`];
+            case 'gt': return [[[vv, params.value]], `${kk} > ${vv}`];
+            case 'ge': return [[[vv, params.value]], `${kk} >= ${vv}`];
+            case 'between': return [[[`${vv}a`, params.lowerBound], [`${vv}b`, params.upperBound]], `${kk} BETWEEN ${vv}a AND ${vv}b`];
+        }
+    }
+    return [[[`:v${i}`, v]], `${kk} = :v${i}`];
+};
 const buildParams = (table, key, returns = undefined, extra = {}) => ({TableName: buildTableName(table), Key: key, ...buildReturns(returns), ...extra});
-const buildExpressionParams = (filter, prefix, first, between) =>
+const buildExpressionParams = (filter, prefix, first, between, operationMode = true) =>
     (!filter || !Object.keys(filter).length)
         ? {}
         : Object.entries(filter).reduce((acc, [k, v], i) => {
             const kk = `#K${i}`;
-            const vv = `:v${i}`;
+            const [vvv, op] = operationMode ? buildOperationString(v, kk, i) : [[[`:v${i}`, v]], `${kk} = :v${i}`];
             acc.ExpressionAttributeNames[kk] = k;
-            acc.ExpressionAttributeValues[vv] = v;
-            acc[`${prefix}Expression`] = `${acc[`${prefix}Expression`] || ''}${acc[`${prefix}Expression`] ? between : first}${kk} = ${vv}`;
+            vvv.forEach(vv => {
+                acc.ExpressionAttributeValues[vv[0]] = vv[1];
+            });
+            acc[`${prefix}Expression`] = `${acc[`${prefix}Expression`] || ''}${acc[`${prefix}Expression`] ? between : first}${op}`;
             return acc;
         }, {ExpressionAttributeNames: {}, ExpressionAttributeValues: {}, [`${prefix}Expression`]: undefined})
 ;
 const buildUpdateParams = (table, key, data, returns = undefined) => ({
     ...buildParams(table, key, returns),
-    ...buildExpressionParams(data, 'Update', 'SET ', ', '),
+    ...buildExpressionParams(data, 'Update', 'SET ', ', ', false),
 });
 
-const buildQueryParams = (table, index, key) => ({
+const buildQueryParams = (table, key, index = undefined) => ({
     TableName: buildTableName(table),
-    IndexName: index,
+    ...(index ? {IndexName: index} : {}),
     ...buildExpressionParams(key, 'KeyCondition', '', ' and '),
 });
 const buildScanParams = (table, filter = {}, index = undefined) => ({
@@ -52,7 +70,8 @@ const mutateResult = r => ({
 module.exports = {
     delete: async (table, key, returns = undefined) => db.delete(buildParams(table, key, returns)).promise(),
     upsert: async (table, key, data, returns = undefined) => db.update(buildUpdateParams(table, key, data, returns)).promise(),
-    queryIndex: async (table, index, key) => mutateResult(await db.query(buildQueryParams(table, index, key)).promise()),
+    queryIndex: async (table, index, key) => mutateResult(await db.query(buildQueryParams(table, key, index)).promise()),
+    query: async (table, key) => mutateResult(await db.query(buildQueryParams(table, key)).promise()),
     scanIndex: async (table, index, filter = {}) => mutateResult(await db.scan(buildScanParams(table, filter, index)).promise()),
     scan: async (table, filter = {}) => mutateResult(await db.scan(buildScanParams(table, filter)).promise()),
 };
