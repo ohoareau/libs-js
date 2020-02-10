7a6178c3c9c152c9e7491c18036f63248ba4a525
Olivier Hoareau <oha@greenberets.io>
2020-02-10 22:55:11 +0100

    fix

--- packages/microgen/src/Package.ts
+++ packages/microgen/src/Package.ts
@@ -62,7 +62,7 @@ export default class Package {
                 name: vars.name,
                 license: vars.license,
                 dependencies: {
-                    '@ohoareau/microlib': '^0',
+                    '@ohoareau/microlib': '^0.1.2',
                     ...(vars.dependencies || {}),
                 },
                 scripts: {
