664d0b07110ad83c8879c516cdc5d42d5ffb0ddc
Olivier Hoareau <oha@greenberets.io>
2020-02-11 09:43:00 +0100

    fix

--- packages/microlib/src/middlewares/error.ts
+++ packages/microlib/src/middlewares/error.ts
@@ -1,10 +1,11 @@
-export default () => next => async action => {
+export default c => next => async action => {
     try {
         const r = await next(action) || {};
         r.response = r.response || {};
         r.response.result = await r.response.result;
         return r;
     } catch (e) {
+        c = undefined;
         return (('object' === typeof e) && e.serialize)
             ? e.serialize()
             : {...action, response: {...(action.response || {}), result: {errorType: e.name || 'Error', message: e.message || 'Error', data: {}, errorInfo: {}}}}
--- packages/microlib/src/middlewares/warmup.ts
+++ packages/microlib/src/middlewares/warmup.ts
@@ -1,4 +1,6 @@
-export default () => next => async action =>
-    (action.request['event'] && action.request['event']['warm'])
+export default c => next => async action => {
+    c = undefined;
+    return (action.request['event'] && action.request['event']['warm'])
         ? {...action, response: {...(action.response || {}), result: {status: 'success', code: 1000, message: 'warmed'}}}
-        : next(action)
+        : next(action);
+}
