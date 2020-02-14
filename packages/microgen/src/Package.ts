5aba9f16162000025e84a11d061b74e987d6e440
Olivier Hoareau <oha@greenberets.io>
2020-02-14 10:42:18 +0100

    microlib: add missing dependency to migrate package

--- packages/microgen/src/Package.ts
+++ packages/microgen/src/Package.ts
@@ -62,7 +62,7 @@ export default class Package {
                 name: vars.name,
                 license: vars.license,
                 dependencies: {
-                    '@ohoareau/microlib': '^0.4.4',
+                    '@ohoareau/microlib': '^0.4.5',
                     ...(vars.dependencies || {}),
                 },
                 scripts: {
