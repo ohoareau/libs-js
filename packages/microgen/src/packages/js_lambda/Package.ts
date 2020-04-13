93334b8930e1cd43844b0c978d972dc86dd31da8
Olivier Hoareau <oha@greenberets.io>
2020-04-13 15:03:19 +0200

    fix

--- packages/microgen/src/packages/js_lambda/Package.ts
+++ packages/microgen/src/packages/js_lambda/Package.ts
@@ -76,7 +76,7 @@ export default class Package {
                 name: vars.name,
                 license: vars.license,
                 dependencies: {
-                    '@ohoareau/microlib': '^0.6.46',
+                    '@ohoareau/microlib': '^0.6.47',
                     ...(vars.dependencies || {}),
                 },
                 scripts: {
