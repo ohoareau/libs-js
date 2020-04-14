781e0fcef49e15d175cb71eef633e59e051a3d1d
Olivier Hoareau <oha@greenberets.io>
2020-04-14 09:51:27 +0200

    react-dynamic: add missing export

--- packages/react-dynamic/src/index.ts
+++ packages/react-dynamic/src/index.ts
@@ -2,3 +2,4 @@ export {default as Form} from './Form';
 export {default as FormField} from './FormField';
 export {default as ModuleTypePanel} from './ModuleTypePanel';
 export {default as ModuleTypeActionModal} from './ModuleTypeActionModal';
+export {default as ModuleTypeActionFormWrapper} from './ModuleTypeActionFormWrapper';
