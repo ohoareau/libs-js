4e1bdbbe4e52d71291f1dc6644a5eeedca1cf66f
Olivier Hoareau <oha@greenberets.io>
2020-05-08 19:03:46 +0200

    microlib: add json2string transformer

--- packages/microlib/src/transformers.ts
+++ packages/microlib/src/transformers.ts
@@ -1,3 +1,4 @@
 export const upper = () => v => `${v}`.toUpperCase();
 export const lower = () => v => `${v}`.toLowerCase();
+export const json2string = () => v => JSON.stringify(v);
 export const password = ({rounds}) => v => require('bcryptjs').hashSync(v, rounds);
