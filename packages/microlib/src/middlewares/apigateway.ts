60d83345165a270db1b0c0a9cf3b5b108adb6d5c
Olivier Hoareau <oha@greenberets.io>
2020-06-09 10:17:28 +0200

    microlib: Fixes #31 api gateway v2 support (#32)

--- packages/microlib/src/middlewares/apigateway.ts
+++ packages/microlib/src/middlewares/apigateway.ts
@@ -1,4 +1,4 @@
-export default () => async (req, res, next) => {
+export const populateFromV1 = (req, res) => {
     req.method = req.httpMethod;
     req.path = req.resource;
     req.params = {
@@ -9,5 +9,30 @@ export default () => async (req, res, next) => {
     Object.assign(req, JSON.parse(req.body || '{}'));
     res.type('application/json');
     res.bodyOnly = false;
+};
+
+export const populateFromV2 = (req, res) => {
+    req.method = req.requestContext.http.method;
+    req.path = req.requestContext.http.path;
+    req.params = {
+        ...(req.params || {}),
+        ...(req.queryStringParameters || {}),
+        ...(req.pathParameters || {}),
+    };
+    Object.assign(req, JSON.parse(req.body || '{}'));
+    res.type('application/json');
+    res.bodyOnly = false;
+};
+
+export const detectVersion: (any) => 'v1' | 'v2' = (req: any) =>
+    (req.requestContext && req.requestContext.http) ? 'v2' : 'v1'
+;
+
+export default () => async (req, res, next) => {
+    switch (detectVersion(req)) {
+        case 'v2': populateFromV2(req, res); break;
+        default:
+        case 'v1': populateFromV1(req, res); break;
+    }
     return next();
 }
