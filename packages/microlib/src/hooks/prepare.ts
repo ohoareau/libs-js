185ac630f0a6910206306a8d7d7f367acb8fc4d5
Olivier Hoareau <oha@greenberets.io>
2020-07-10 16:29:10 +0200

    microlib: fix non deleting volatileData on query when existing

--- packages/microlib/src/hooks/prepare.ts
+++ packages/microlib/src/hooks/prepare.ts
@@ -1,5 +1,5 @@
 export default ({model: {volatileFields}}) => async data => {
-    data.volatileData = {};
+    data.volatileData = !!data.volatileData ? data.volatileData : {};
     Object.entries(data.data).forEach(([k, v]) => {
         if (volatileFields[k]) {
             data.volatileData[k] = v;
