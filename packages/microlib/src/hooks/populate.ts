3f6ea7edbcf77f68728fb230576db28da0e9cbca
Olivier Hoareau <oha@greenberets.io>
2020-09-27 23:31:27 +0200

    fix(microlib): more clever populate hooks

--- packages/microlib/src/hooks/populate.ts
+++ packages/microlib/src/hooks/populate.ts
@@ -23,19 +23,42 @@ export default ({model, dir, prefix = undefined}) => async data => {
     const defaultValuesKey = prefix ? `${prefix}DefaultValues` : 'defaultValues';
     const cascadeValuesKey = 'cascadeValues';
     let v;
+    const processed = {values: {}, defaultValues: {}, cascadeValues: {}};
+    // first pass of setting values (auto-populate): only fields that have cascadingValues rules
+    if (model[cascadeValuesKey] && !Object.keys(model[cascadeValuesKey]).length) {
+        Object.entries(model[valuesKey]).forEach(([k, def]) => {
+            if (!model[cascadeValuesKey][k]) return;
+            v = buildValueGenerator(<any>def, dir)(data);
+            if ('**unchanged**' !== v) data.data[k] = v;
+            processed.values[k] = true;
+        });
+        Object.entries(model[defaultValuesKey]).forEach(([k, def]) => {
+            if (!model[cascadeValuesKey][k]) return;
+            processed.defaultValues[k] = true;
+            if (data.data[k]) return;
+            v = buildValueGenerator(<any>def, dir)(data);
+            if ('**unchanged**' !== v) data.data[k] = v;
+        });
+        Object.entries(model[cascadeValuesKey]).forEach(([k, def]) => {
+            const matchCase = findCascadeMatchingCase(data.data[k], def);
+            processed.cascadeValues[k] = true;
+            if (!matchCase) return;
+            Object.assign(data.data, buildCascadeValues(matchCase, data, dir));
+        });
+    }
+    // second pass: settings values (auto-populate): only fields that have not already been populated (above)
     Object.entries(model[valuesKey]).forEach(([k, def]) => {
+        if (processed.values[k]) return;
         v = buildValueGenerator(<any>def, dir)(data);
         if ('**unchanged**' !== v) data.data[k] = v;
+        processed.values[k] = true;
     });
     Object.entries(model[defaultValuesKey]).forEach(([k, def]) => {
+        if (processed.defaultValues[k]) return;
+        processed.defaultValues[k] = true;
         if (data.data[k]) return;
         v = buildValueGenerator(<any>def, dir)(data);
         if ('**unchanged**' !== v) data.data[k] = v;
-    });
-    Object.entries(model[cascadeValuesKey]).forEach(([k, def]) => {
-        const matchCase = findCascadeMatchingCase(data.data[k], def);
-        if (!matchCase) return;
-        Object.assign(data.data, buildCascadeValues(matchCase, data, dir));
     });
     return data;
 }
