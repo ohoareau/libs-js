4666e08aa9189cb68acb5495461bd56103d33ba1
Olivier Hoareau <oha@greenberets.io>
2020-09-28 09:04:40 +0200

    feat(react-data-table): more options for action buttons

--- packages/react-data-table/src/DataTableBodyRowCellAction.tsx
+++ packages/react-data-table/src/DataTableBodyRowCellAction.tsx
@@ -10,7 +10,7 @@ const DataTableBodyRowCellAction = component<DataTableBodyRowCellActionProps>({
     button: {
         marginRight: 5,
     },
-}, ({buttonComponent, classes = {}, t = () => {}, icon, tReady = false, expandedLabel, label, expanded, type, onClick, row, confirmable = false}: DataTableBodyRowCellActionProps) => {
+}, ({buttonComponent, classes = {}, t = () => {}, icon, tReady = false, expandedLabel, label, buttonVariant = 'outlined', buttonColor = 'inherit', buttonSize = 'small', startIcon, expandedStartIcon, expanded, type, onClick, row, confirmable = false}: DataTableBodyRowCellActionProps) => {
     const Button: any = buttonComponent;
     const confirm = useConfirm();
     const handleConfirmableClick = e => {
@@ -28,15 +28,18 @@ const DataTableBodyRowCellAction = component<DataTableBodyRowCellActionProps>({
     const handleNonConfirmableClick = useCallback(e => {e.stopPropagation(); onClick && onClick(row)}, [onClick, row]);
     const handleClick = confirmable ? handleConfirmableClick : handleNonConfirmableClick;
     const IconComponent = icon || DeleteIcon;
+    const StartIcon = startIcon || KeyboardArrowDownIcon;
+    const ExpandedStartIcon = expandedStartIcon || KeyboardArrowUpIcon;
+    const realStartIcon = false === startIcon ? undefined : (expanded ? <ExpandedStartIcon/> : <StartIcon />);
     return (
         <div>
             {('icon' === type) && (
-                <IconButton style={{marginLeft: 5}} color="secondary" size="small" onClick={handleClick}>
+                <IconButton style={{marginLeft: 5}} color={buttonColor as any} size={buttonSize as any} onClick={handleClick}>
                     <IconComponent />
                 </IconButton>
             )}
             {('icon' !== type) && (
-                <Button variant="outlined" color="inherit" size="small" className={classes.button} onClick={handleClick} startIcon={expanded ? <KeyboardArrowUpIcon/> : <KeyboardArrowDownIcon/>} label={t(expanded ? (expandedLabel || label) : label)} />
+                <Button variant={buttonVariant} color={buttonColor} size={buttonSize} className={classes.button} onClick={handleClick} startIcon={realStartIcon} label={t(expanded ? (expandedLabel || label) : label)} />
             )}
         </div>
     );
@@ -55,6 +58,11 @@ export interface DataTableBodyRowCellActionProps {
     onClick?: Function,
     row?: any,
     confirmable?: boolean,
+    startIcon?: any,
+    expandedStartIcon?: any,
+    buttonVariant?: string,
+    buttonColor?: string,
+    buttonSize?: string,
 }
 
 export default DataTableBodyRowCellAction
