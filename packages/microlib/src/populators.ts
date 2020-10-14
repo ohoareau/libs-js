cbb7287cb40b7e268656581630755a5d8d94afa1
Olivier Hoareau <oha@greenberets.io>
2020-10-14 10:58:13 +0200

    feat(microlib): auto populated fields (private) are now marked as autopopulated and not cleaned/deleted in the validation phase

--- packages/microlib/src/populators.ts
+++ packages/microlib/src/populators.ts
@@ -5,8 +5,15 @@ export const user_id = () => ({user}: any = {}) => (user && user.id) ? user.id :
 export const uuid = () => () => require('uuid').v4();
 export const now = () => () => new Date().valueOf();
 export const ref_attribute_field = ({key, prefix, sourceField}) => ({data, contextData}) => {
-    if (!data || !data[key]) return '**unchanged**';
-    return (contextData[`${prefix}.${data[key]}`] || {})[sourceField] || undefined;
+    if (!data) return '**unchanged**';
+    if (key in data) {
+        if (('**clear**' === data[key]) || (undefined === data[key])) {
+            return '**clear**';
+        }
+        if (!data[key]) return '**unchanged**';
+        return (contextData[`${prefix}.${data[key]}`] || {})[sourceField] || undefined;
+    }
+    return '**unchanged**'
 };
 export const empty = () => () => undefined;
 export const value = ({value}) => () => value;
