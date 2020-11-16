d5feda03d0e6a9dc64af9eaaa14ad7ea91a881be
Michel MAIER <michel.maier@gmail.com>
2020-11-16 09:00:18 +0100

    feat(dynaform): implements json schema validation

--- packages/dynaform/__tests__/index.spec.ts
+++ packages/dynaform/__tests__/index.spec.ts
@@ -1,19 +1,20 @@
+jest.mock('../src/formats', () => ({
+    __esModule: true,
+    default: {
+        '9.9': class {validate(){}},
+    }
+}));
+
 import {validate} from '../src';
 
 describe('validate', () => {
     it('throw error for object with no format version', () => {
         expect(() => validate({})).toThrowError(new Error('No format version specified'));
     });
-    it('do not throw errors if known format specified but no other definition', () => {
-        expect(() => validate({format: '1.0'})).not.toThrow();
-    });
     it('throw error if format specified but unknown format', () => {
         expect(() => validate({format: '0.0'})).toThrow(new Error('Unknown format version 0.0'));
     });
-    it('throw error if underlying format validation throw error', () => {
-        expect(() => validate({
-            format: '1.0',
-            models: {field1: {}},
-        })).toThrow(new Error('Validation error (1 violation)'));
+    it('pass on ', () => {
+        expect(() => validate({format: '9.9'})).not.toThrow();
     });
 });
