d433fda253c42171040e05a86be4f089a9300822
Rodolphe Eveilleau <rodolphe@eveilleau.fr>
2020-12-03 17:46:36 +0100

    add attribute 'comp' on the 'columns' property (#50)
    * add attribute 'component' on the 'columns' property
    
    * Update DataTableBodyRowCell.tsx
    
    Co-authored-by: Olivier Hoareau <oha@greenberets.io>

--- packages/react-data-table/src/DataTableBodyRowCell.tsx
+++ packages/react-data-table/src/DataTableBodyRowCell.tsx
@@ -15,71 +15,82 @@ const DataTableBodyRowCell: ComponentType<DataTableBodyRowCellProps> = ({buttonC
         extraProps.component = 'th';
         extraProps.scope = 'row';
     }
-    switch (column.type || column.id) {
-        case 'status':
-            extraProps.padding = 'checkbox';
-            extraProps.style = {...extraProps.style, width: 50};
-            const v = row.status;
-            switch (v) {
-                case 'CREATED':
-                    content = <FiberManualRecordIcon />;
-                    break;
-                case 'STARTED':
-                    content = <CircularProgress size={20} />;
-                    break;
-                case 'COMPLETED':
-                    content = <CheckCircleIcon />;
-                    break;
-                case 'ERRORED':
-                    content = <ErrorOutlineIcon />;
-                    break;
-                default:
-                    content = column.format ? column.format(row[column.id], row) : row[column.id];
-                    break;
-            }
-            break;
-        case 'selector':
-            extraProps.padding = 'checkbox';
-            content = <Checkbox inverted={true} color={'primary'} checked={selected} inputProps={{'aria-labelledby': `enhanced-table-checkbox-${row.id}`}} />;
-            break;
-        case 'actions':
-            extraProps.padding = 'checkbox';
-            extraProps.style = {...extraProps.style, width: (actions.length > 1) ? 160 : 130};
-            content = (
-                <div style={{display: 'flex'}}>
-                    {(actions as any[]).map((action) => <DataTableBodyRowCellAction key={action.id} buttonComponent={buttonComponent} {...action} expanded={expanded} row={row} />)}
-                </div>
-            );
-            break;
-        case 'details':
-            extraProps.padding = 'none';
-            content = (
-                <div style={{padding: 20}}>
-                    {children}
-                </div>
-            );
-            break;
-        case 'title':
-            content = (
-                <div style={{padding: '10px 0 10px 0', fontWeight: 'bold'}}>{column.format ? column.format(row[column.id], row) : row[column.id]}</div>
-            );
-            break;
-        case 'comparison_title':
-            extraProps.padding = 'checkbox';
-            content = (
-                <div style={{padding: '10px 0 10px 0', fontWeight: 'bold', textAlign: 'right'}}>{column.format ? column.format(row[column.id], row) : row[column.id]}</div>
-            );
-            break;
-        case 'centered':
-            content = (
-                <div style={{textAlign: 'center'}}>{column.format ? column.format(row[column.id], row) : row[column.id]}</div>
-            );
-            break;
-        default:
-            content = column.format ? column.format(row[column.id], row) : row[column.id];
-            break;
+
+    let content = undefined;
+    
+    if (column.component) {
+        const Component = column.component;
+        content = <Component>{row[column.id]}</Component>;
+    } else {
+        switch (column.type || column.id) {
+            case 'status':
+                extraProps.padding = 'checkbox';
+                extraProps.style = {...extraProps.style, width: 50};
+                const v = row.status;
+                switch (v) {
+                    case 'CREATED':
+                        content = <FiberManualRecordIcon />;
+                        break;
+                    case 'STARTED':
+                        content = <CircularProgress size={20} />;
+                        break;
+                    case 'COMPLETED':
+                        content = <CheckCircleIcon />;
+                        break;
+                    case 'ERRORED':
+                        content = <ErrorOutlineIcon />;
+                        break;
+                    default:
+                        content = column.format ? column.format(row[column.id], row) : row[column.id];
+                        break;
+                }
+                break;
+            case 'selector':
+                extraProps.padding = 'checkbox';
+                content = <Checkbox inverted={true} color={'primary'} checked={selected} inputProps={{'aria-labelledby': `enhanced-table-checkbox-${row.id}`}} />;
+                break;
+            case 'actions':
+                extraProps.padding = 'checkbox';
+                extraProps.style = {...extraProps.style, width: (actions.length > 1) ? 160 : 130};
+                content = (
+                    <div style={{display: 'flex'}}>
+                        {(actions as any[]).map((action) => <DataTableBodyRowCellAction key={action.id} buttonComponent={buttonComponent} {...action} expanded={expanded} row={row} />)}
+                    </div>
+                );
+                break;
+            case 'details':
+                extraProps.padding = 'none';
+                content = (
+                    <div style={{padding: 20}}>
+                        {children}
+                    </div>
+                );
+                break;
+            case 'title':
+                content = (
+                    <div style={{padding: '10px 0 10px 0', fontWeight: 'bold'}}>{column.format ? column.format(row[column.id], row) : row[column.id]}</div>
+                );
+                break;
+            case 'comparison_title':
+                extraProps.padding = 'checkbox';
+                content = (
+                    <div style={{padding: '10px 0 10px 0', fontWeight: 'bold', textAlign: 'right'}}>{column.format ? column.format(row[column.id], row) : row[column.id]}</div>
+                );
+                break;
+            case 'centered':
+                content = (
+                    <div style={{textAlign: 'center'}}>{column.format ? column.format(row[column.id], row) : row[column.id]}</div>
+                );
+                break;
+            default:
+                content = column.format ? column.format(row[column.id], row) : row[column.id];
+                break;
+        }
     }
-    return <TableCell {...extraProps}>{content}</TableCell>;
+    
+    const RootComponent = row.rootComponent || TableCell;
+
+    return <RootComponent {...extraProps}>{content}</RootComponent>;
 };
 
 export interface DataTableBodyRowCellProps {
