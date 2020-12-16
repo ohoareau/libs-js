cf21211be9eb747259f629416b7e8757a2af936e
Olivier Hoareau <oha@greenberets.io>
2020-12-16 11:56:17 +0100

    feat(apollo-client-jwt): creation

---
+++ packages/apollo-client-jwt/src/index.ts
@@ -0,0 +1,48 @@
+import decodeJwt from 'jwt-decode';
+import ApolloLinkTimeout from 'apollo-link-timeout';
+import {onError} from '@apollo/client/link/error';
+import {setContext} from '@apollo/client/link/context';
+import {ApolloClient, InMemoryCache, createHttpLink} from '@apollo/client';
+
+export const createClient = ({getCurrentTokens, setCurrentTokens, refreshTokens, onLogout, onAuthError, uri, timeout = 5000}) => {
+    const authClient = new ApolloClient({
+        link: onError(() => {}).concat(new ApolloLinkTimeout(timeout).concat(createHttpLink({uri}))),
+        cache: new InMemoryCache(),
+        defaultOptions: {query: {fetchPolicy: 'no-cache'}},
+    });
+    const authLink = setContext(async (_, { headers }) => {
+        let {token, refreshToken} = await getCurrentTokens();
+        if (!token) {
+            onLogout && await onLogout();
+            return;
+        }
+        const decodedToken = decodeJwt(token) as any;
+        const now = Math.floor(Date.now() / 1000);
+        if (((decodedToken || {}).exp - now) < 1) {
+            try {
+                if (!refreshToken) {
+                    onAuthError && await onAuthError(new Error(`No refresh-token available`));
+                    return;
+                }
+                const tokenData = await refreshTokens(refreshToken, authClient);
+                if (!tokenData || !tokenData.refreshToken || !tokenData.token) {
+                    // noinspection ExceptionCaughtLocallyJS
+                    throw new Error(`Unable to refresh the token (jwt expired ?)`);
+                }
+                await setCurrentTokens(tokenData);
+                token = tokenData.token;
+            } catch (e) {
+                onLogout && await onLogout();
+                throw e;
+            }
+        }
+        return {headers: {...headers, authorization: token ? `Bearer ${token}` : ''}};
+    });
+    return new ApolloClient({
+        link: onError(() => {}).concat(authLink.concat(new ApolloLinkTimeout(timeout)
+            .concat(createHttpLink({uri})))),
+        cache: new InMemoryCache({}),
+    });
+};
+
+export default createClient
