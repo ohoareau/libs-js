638187a2a39d10127682683f897350dea5bcb95a
Olivier Hoareau <oha@greenberets.io>
2021-01-10 22:36:52 +0100

    fix: fix

--- packages/aws-handlers/src/factories/s3event.ts
+++ packages/aws-handlers/src/factories/s3event.ts
@@ -1,7 +1,8 @@
 import {s3} from '@ohoareau/aws';
 import {createRunner} from '@ohoareau/topper';
 
-const plugin = (source, {plugins, targetBucket = undefined, processedDir = 'archived/processed', errorsDir = 'archived/errors'}) => async (bucket, key) => {
+const plugin = (source, {run, ruleName, plugins, targetBucket = undefined, processedDir = 'archived/processed/:rule', errorsDir = 'archived/errors/:rule'}) => async (bucket, key) => {
+    ruleName = ruleName || 'unknown-rule';
     targetBucket = targetBucket || bucket;
     let p = plugins[source];
     ('function' === typeof p) && (p = {
@@ -10,11 +11,11 @@ const plugin = (source, {plugins, targetBucket = undefined, processedDir = 'arch
         execute: p,
         clean: <Function>(async () => {}),
     });
-    const run = createRunner();
+    processedDir = processedDir.replace(':rule', ruleName);
+    errorsDir = errorsDir.replace(':rule', ruleName);
     const getData = async () => s3.getFileContent({bucket, key, raw: true});
     const getTargetObject = to => ({bucket: targetBucket, key: `${to ? to : ''}${to ? '/' : ''}${key}`});
     const copyFileFactory = to => async () => s3.setFileContent(getTargetObject(to), await getData());
-    const deleteFile = async () => s3.deleteFile({bucket, key});
     const ctx = await p.init({source, run, log: console.log, error: console.error});
     const processedTargetObject = getTargetObject(processedDir);
     const errorsTargetObject = getTargetObject(errorsDir);
@@ -24,7 +25,7 @@ const plugin = (source, {plugins, targetBucket = undefined, processedDir = 'arch
     const defaults = {getData, bucket, key, source};
     const originalPath = formatObjectPath({bucket, key});
     await run(
-        ['process', originalPath],
+        ['process', `rule: ${ruleName}, file: ${originalPath}`],
         async () => {
             p.load && (await run('load', async () => p.load({...defaults}, ctx)));
             p.execute && (await run('execute', async () => p.execute({...defaults}, ctx)));
@@ -37,12 +38,11 @@ const plugin = (source, {plugins, targetBucket = undefined, processedDir = 'arch
         },
         async () => {
             p.clean && (await p.clean({...defaults}, ctx));
-            await run('clean', deleteFile);
         }
     );
 };
 
-const consumeS3 = ({eventType = 'ObjectCreated', plugins = undefined, rules = [], ...rest}: {eventType?: string, plugins?: any, rules?: any[]}) => async record => {
+const consumeS3 = ({eventType = 'ObjectCreated', clean = true, plugins = undefined, rules = [], ...rest}: {clean?: boolean, eventType?: string, plugins?: any, rules?: any[]}) => async record => {
     const bucket = record.s3.bucket['name'];
     const key = record.s3.object.key;
     const isHandledEventRecord = new RegExp(`^${eventType}`).test(record.eventName);
@@ -51,6 +51,7 @@ const consumeS3 = ({eventType = 'ObjectCreated', plugins = undefined, rules = []
         console.log(`Ignoring non-handled object '${key}' from bucket '${bucket}'.`)
         return;
     }
+    const run = createRunner();
     const hits = rules.reduce((acc, rule) => {
         const patterns = rule.match ? (Array.isArray(rule.match) ? rule.match : [rule.match]) : [];
         const ignorePatterns = rule.ignore ? (Array.isArray(rule.ignore) ? rule.ignore : [rule.ignore]) : [];
@@ -59,14 +60,30 @@ const consumeS3 = ({eventType = 'ObjectCreated', plugins = undefined, rules = []
         foundPatternMatch && !foundIgnorePatternMatch && acc.push({rule, match: foundPatternMatch});
         return acc;
     }, []);
-    await Promise.all(hits.map(async hit => {
-        let fn = hit.rule.function;
-        if (hit.rule.plugin && plugins) {
-            fn = plugin(hit.rule.plugin, {...rest, plugins})
-        }
-        return (fn as any)(bucket, hit.match);
-    }));
-    !hits.length && console.log(`incoming ${bucket}:${key} => IGNORED`);
+    let error = undefined;
+    try {
+        await Promise.all(hits.map(async hit => {
+            let fn = hit.rule.function;
+            let localPlugins = plugins;
+            if (hit.rule.plugin) {
+                let pName = hit.rule.plugin;
+                if ('string' !== typeof pName) {
+                    localPlugins = {default: pName};
+                    pName = 'default';
+                }
+                fn = plugin(pName, {...rest, run, ruleName: hit.rule.name, plugins: localPlugins})
+            }
+            return (fn as any)(bucket, hit.match);
+        }));
+    } catch (e) {
+        error = e;
+    }
+    if (hits.length) {
+        clean && (await run('clean', async () => s3.deleteFile({bucket, key})));
+    } else {
+        console.log(`incoming ${bucket}:${key} => IGNORED`);
+    }
+    if (error) throw error;
 };
 
 const consumerFactories = config => ({
