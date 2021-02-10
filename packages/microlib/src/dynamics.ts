26b307864553902a2264c3bb55a23ec22825c47f
Olivier Hoareau <oha@greenberets.io>
2021-02-10 15:44:02 +0100

    fix(microlib): dynamics

--- packages/microlib/src/dynamics.ts
+++ packages/microlib/src/dynamics.ts
@@ -1,5 +1,46 @@
 export const uuid = () => () => require('uuid').v4();
-export const pattern = ({pattern}) => data => {
+export const value = ({value}) => () => value;
+export const s3 = ({s3}) => async data => {
+    let [bucket, key] = s3.split(':');
+    bucket = replaceVars(bucket, data);
+    key = replaceVars(key, data);
+    return require('@ohoareau/aws').s3.getFileContent({bucket, key});
+}
+export const s3url_dl = ({s3url_dl, s3url = undefined}) => async data => {
+    let [bucket, key, name = undefined] = (s3url_dl || s3url || '').split(':');
+    bucket = replaceVars(bucket, data);
+    key = replaceVars(key, data);
+    name = name ? replaceVars(name, data) : undefined;
+    return require('@ohoareau/aws').s3.getFileDownloadUrl({bucket, key, name});
+}
+export const s3url_view = ({s3url_view, s3url = undefined}) => async data => {
+    let [bucket, key, contentType = undefined] = (s3url_view || s3url || '').split(':');
+    bucket = replaceVars(bucket, data);
+    key = replaceVars(key, data);
+    contentType = contentType ? replaceVars(contentType, data) : undefined;
+    return require('@ohoareau/aws').s3.getFileViewUrl({bucket, key, contentType});
+}
+export const s3url = s3url_view;
+export const s3url_ul = ({s3url_ul, s3url = undefined}) => async data => {
+    let [bucket, key, expires = undefined] = (s3url_ul || s3url || '').split(':');
+    bucket = replaceVars(bucket, data);
+    key = replaceVars(key, data);
+    expires = replaceVars(`${expires}`, data);
+    if ((expires === '') || (expires === '-1') || (expires === 'undefined') || (expires === 'false')) {
+        expires = undefined
+    } else {
+        expires = parseInt(expires);
+    }
+    return require('@ohoareau/aws').s3.getFileUploadUrl({bucket, key, expires});
+}
+export const pattern = ({pattern}) => data => replaceVars(pattern, data);
+export const url = ({url}) => data => replaceVars(url, data);
+export const operation = ({operation, dir}) => async (data, query) => {
+    operation = replaceVars(operation, data);
+    return require('./services/caller').default.execute(operation, [{...query, ...data}], `${dir}/services/crud`);
+}
+
+function replaceVars(pattern, data = {}) {
     const envVarMatch = pattern.match(/^\[\[process.env.([^\]]+)]]$/);
     if (envVarMatch) {
         pattern = process.env[envVarMatch[1]] || '';
@@ -7,7 +48,7 @@ export const pattern = ({pattern}) => data => {
 
     const r = new RegExp('\{\{([^\}]+)\}\}');
     const matches = [...pattern.matchAll(r)];
-    const getValue = k => data[k];
+    const getValue = k => ('undefined' === typeof data[k]) ? '' : data[k];
 
     return matches.reduce((acc, m) => {
         for (let i = 0; i < (m.length - 1); i++) {
