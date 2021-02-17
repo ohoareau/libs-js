aa7ace7fcfc61d34e8fcf8a7056303a7c1b1fec3
Olivier Hoareau <oha@greenberets.io>
2021-02-17 12:12:49 +0100

    fix(lambda-api-image): fix

--- packages/lambda-api-image/__tests__/index.spec.ts
+++ packages/lambda-api-image/__tests__/index.spec.ts
@@ -1,5 +1,27 @@
-describe('', () => {
-    it('', () => {
-        expect(true).toBeTruthy();
+process.env.LAMBDA_TASK_ROOT = `${__dirname}/../__fixtures__/lambda-root`;
+import createHandler from '../src';
+
+describe('createHandler', () => {
+    it('GET /robots.txt', async () => {
+        const handler = createHandler();
+        await expect(handler({requestContext: {http: {path: '/robots.txt'}}}, {})).resolves.toEqual({
+            body: require('fs').readFileSync(`${__dirname}/../__fixtures__/lambda-root/statics/robots.txt`, null).toString('base64'),
+            headers: {
+                'Content-Type': 'text/plain',
+            },
+            isBase64Encoded: true,
+            statusCode: 200,
+        });
+    })
+    it('GET /', async () => {
+        const handler = createHandler();
+        await expect(handler({requestContext: {http: {path: '/'}}}, {})).resolves.toEqual({
+            body: JSON.stringify({message: 'not yet implemented'}),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            isBase64Encoded: false,
+            statusCode: 200,
+        });
     })
 })
--- packages/lambda-utils/src/utils/getLambdaConfig.ts
+++ packages/lambda-utils/src/utils/getLambdaConfig.ts
@@ -2,7 +2,12 @@ let config: any|undefined = undefined;
 const root = process.env.LAMBDA_TASK_ROOT;
 
 export async function getLambdaConfig() {
-    return config || {root, ...(await require(`${root}/config`)())};
+    if (config) return config;
+
+    config = config || {root, ...(await require(`${root}/config`)())};
+    config.statics = {...(config.statics || {}), ...(await require(`${root}/config-statics`)())}
+
+    return config;
 }
 
 export default getLambdaConfig
