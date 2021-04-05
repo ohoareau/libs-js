6ae84937a6e1bad32db921a90cf2afd33b1246c9
Olivier Hoareau <oha@greenberets.io>
2021-04-05 18:28:49 +0200

    feat(aws-apigw-next): tests

--- packages/aws-apigw-next/src/index.ts
+++ packages/aws-apigw-next/src/index.ts
@@ -2,10 +2,16 @@ import wrapper from '@ohoareau/aws-apigw-reqres';
 import next from 'next';
 import {parse} from 'url';
 
-export default wrapper(
-    (req, res) => next({dev: process.env.NODE_ENV !== 'production'}).getRequestHandler()(
+export const handler = wrapper(
+    (req, res) => next({
+        dev: (process.env.NODE_ENV !== 'production') && !process.env.AWS_NEXT_PRODUCTION,
+        dir: process.env.AWS_NEXT_PROJECT_DIR || '.',
+        quiet: !!process.env.AWS_NEXT_VERBOSE,
+    }).getRequestHandler()(
         req,
         res,
         parse(req.url, true)
     )
-)
+);
+
+export default handler
