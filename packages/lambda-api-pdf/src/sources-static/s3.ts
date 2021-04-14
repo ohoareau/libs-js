31f9b0c93f9d6d1598a64902a6fc293f21ec80e4
Olivier Hoareau <oha@greenberets.io>
2021-04-14 22:49:22 +0200

    fix: fix

--- packages/lambda-api-pdf/src/sources-static/s3.ts
+++ packages/lambda-api-pdf/src/sources-static/s3.ts
@@ -1,8 +1,9 @@
 import {s3 as awss3} from '@ohoareau/aws';
 import {ctx} from '@ohoareau/lambda-utils';
+import {basename} from 'path';
 
 export async function s3(ctx: ctx): Promise<any|undefined> {
-    let {bucket: originalBucket, name} = ctx.query;
+    let {bucket: originalBucket, name, file = undefined} = ctx.query;
     const bucket = (ctx.config?.static_buckets || {})[originalBucket];
     if (!bucket) return undefined;
     const x = {bucket: originalBucket, key: name, ttl: 5 * 60 /* 5 min */ };
@@ -12,7 +13,11 @@ export async function s3(ctx: ctx): Promise<any|undefined> {
         Object.assign(x, r);
     }
     return {
-        location: (await awss3.getFileViewUrl({...x, contentType: 'application/pdf'})).viewUrl,
+        location: (await awss3.getFileViewUrl({
+            ...x,
+            contentType: 'application/pdf',
+            name: file ? basename(file as string) : undefined,
+        })).viewUrl,
     }
 }
 
