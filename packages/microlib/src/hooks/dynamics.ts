69bcbd7c6b115f8a3fe99464e924c88656374fd9
Olivier Hoareau <oha@greenberets.io>
2021-10-08 18:19:16 +0200

    fix(microlib): ability to have dynamics in find/pages

--- packages/microlib/src/hooks/dynamics.ts
+++ packages/microlib/src/hooks/dynamics.ts
@@ -1,4 +1,4 @@
-const buildValueGenerator = ({type, config = {}}, dir) => {
+const buildValueGenerator = ({type, config = {}}, dir): Function|undefined => {
     let g;
     if ('@' === type.substr(0, 1)) {
         g = require('../dynamics');
@@ -9,9 +9,7 @@ const buildValueGenerator = ({type, config = {}}, dir) => {
     return (g[type.replace(/-/g, '_')] || g.empty)({...config, dir});
 };
 
-export default ({model, dir}) => async (result, query) => {
-    const selectedFields = (query.fields && query.fields.length) ? query.fields : Object.keys(model.dynamics || {});
-    const defs = model.dynamics || {};
+async function populateItem(result, query, defs, selectedFields, dir) {
     await Promise.all(selectedFields.map(async k => {
         if (!defs[k]) return;
         const g = buildValueGenerator(<any>defs[k], dir);
@@ -22,5 +20,20 @@ export default ({model, dir}) => async (result, query) => {
         }
         // currently, if the generator/dynamics-type is unknown (does not exist), there is no errors.
     }));
+    return result;
+}
+
+export default ({model, dir}) => async (result, query, mode: string = 'item') => {
+    const selectedFields = (query.fields && query.fields.length) ? query.fields : Object.keys(model.dynamics || {});
+    const defs = model.dynamics || {};
+    switch (mode) {
+        case 'page':
+            result.items = await Promise.all(result.items.map(item => populateItem({...item}, query, defs, selectedFields, dir)))
+            break;
+        default:
+        case 'item':
+            result = await populateItem(result, query, defs, selectedFields, dir)
+            break;
+    }
     return result;
 }
