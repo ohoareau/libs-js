8d0ecea7223ea0a93545ef42947173550113a800
Olivier Hoareau <oha@greenberets.io>
2021-11-19 22:46:39 +0100

    fix(microlib): fix

--- packages/microlib/src/transformers.ts
+++ packages/microlib/src/transformers.ts
@@ -3,28 +3,28 @@ import {replaceVars} from "./utils";
 export const slug = ({sep = '-'}: any = {}) => v => {
     v = v || '';
     v = v.toLowerCase();
-    v = v.replace(/[éèêëęėē]/, 'e');
-    v = v.replace(/[àâªáäãåā]/, 'a');
-    v = v.replace(/[ÿ]/, 'y');
-    v = v.replace(/[ûùüúū]/, 'u');
-    v = v.replace(/[îïìíįī]/, 'i');
-    v = v.replace(/[ôºöòóõøō]/, 'o');
-    v = v.replace(/[çćč]/, 'c');
-    v = v.replace(/[ñń]/, 'n');
-    v = v.replace(/[œ]/, 'oe');
-    v = v.replace(/[æ]/, 'ae');
-    v = v.replace(/[$]/, ' dollar');
-    v = v.replace(/[€]/, ' euro');
-    v = v.replace(/[@]/, ' at');
-    v = v.replace(/[&]/, ' and');
-    v = v.replace(/[§]/, ' s ');
-    v = v.replace(/[%]/, ' percent ');
+    v = v.replace(/[éèêëęėē]/g, 'e');
+    v = v.replace(/[àâªáäãåā]/g, 'a');
+    v = v.replace(/[ÿ]/g, 'y');
+    v = v.replace(/[ûùüúū]/g, 'u');
+    v = v.replace(/[îïìíįī]/g, 'i');
+    v = v.replace(/[ôºöòóõøō]/g, 'o');
+    v = v.replace(/[çćč]/g, 'c');
+    v = v.replace(/[ñń]/g, 'n');
+    v = v.replace(/[œ]/g, 'oe');
+    v = v.replace(/[æ]/g, 'ae');
+    v = v.replace(/[$]/g, ' dollar');
+    v = v.replace(/[€]/g, ' euro');
+    v = v.replace(/[@]/g, ' at');
+    v = v.replace(/[&]/g, ' and');
+    v = v.replace(/[§]/g, ' s ');
+    v = v.replace(/[%]/g, ' percent ');
 
-    v = v.replace(/[a-z0-9-]+/, ' ').replace(/[\s]+/, ' ')
+    v = v.replace(/[a-z0-9-]+/g, ' ').replace(/[\s]+/g, ' ')
     v = v.trim();
-    v = v.replace(/[\s]+/, sep);
+    v = v.replace(/[\s]+/g, sep);
 
-    return v;
+    return !!v ? v : undefined;
 };
 export const truncate = ({length}) => v => (undefined !== length) ? v.slice(0, length) : v;
 export const prefix = ({prefix}) => v => `${prefix || ''}${v}`;
