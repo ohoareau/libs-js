49d4f135c91ce0f823ea0b900822fe37602bc1e6
Olivier Hoareau <oha@greenberets.io>
2021-11-27 22:11:08 +0100

    feat(aws): add eventbridge service

---
+++ packages/aws/src/services/eventbridge.ts
@@ -0,0 +1,39 @@
+const awseb = new (require('aws-sdk/clients/eventbridge'));
+
+export type ebEvent = {
+    type: string;
+    bus?: string;
+    data: object;
+    source: string;
+    time?: any;
+    resources?: string[];
+    traceHeader?: string;
+}
+
+async function putEvents(events: ebEvent[]) {
+    return awseb.putEvents(events.map(e => ({
+        Detail: JSON.stringify(e.data),
+        DetailType: e.type,
+        Source: `${process.env.EVENTBRIDGE_SOURCE_PREFIX || ''}${e.source}`,
+        EventBusName: e.bus,
+        Time: e.time,
+        Resources: e.resources,
+        TraceHeader: e.traceHeader,
+    }))).promise();
+}
+
+async function putEvent(event: ebEvent) {
+    return putEvents([event]);
+}
+
+async function send(type: string, data: object, source: string, options?: Omit<ebEvent, 'type' | 'data' | 'source'>) {
+    return putEvent({...options, type, data, source});
+}
+
+const service = {
+    send,
+    putEvents,
+    putEvent,
+};
+
+export default service;
