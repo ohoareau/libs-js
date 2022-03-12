2e994aafa72e864fcafc13f3fcaecffeba717650
Olivier Hoareau <oha@greenberets.io>
2022-03-12 23:24:19 +0100

    feat(apollo-client-jwt): ability to declare public / anonymous operations

--- packages/apollo-client-jwt/src/index.ts
+++ packages/apollo-client-jwt/src/index.ts
@@ -12,15 +12,16 @@ export const useMutation = baseUseMutation;
 export const ApolloProvider = BaseApolloProvider;
 export const jwtDecode = decodeJwt;
 
-export const createClient = ({fetch = undefined, fetchOptions = undefined, getCurrentTokens, setCurrentTokens, refreshTokens, onLogout, onAuthError, uri, timeout = 5000}: any) => {
+export const createClient = ({publicOperations = {}, fetch = undefined, fetchOptions = undefined, getCurrentTokens, setCurrentTokens, refreshTokens, onLogout, onAuthError, uri, timeout = 5000}: any) => {
     const authClient = new ApolloClient({
         link: onError(() => {}).concat(new ApolloLinkTimeout(timeout).concat(createHttpLink({uri, fetch, fetchOptions}))),
         cache: new InMemoryCache(),
         defaultOptions: {query: {fetchPolicy: 'no-cache'}},
     });
-    const authLink = setContext(async (_, { headers }) => {
+    const authLink = setContext(async (req, { headers }) => {
         let {token, refreshToken} = await getCurrentTokens();
         if (!token) {
+            if (!!publicOperations && !!publicOperations[req.operationName || '']) return {headers}; // non-authentified is allowed for this operation
             onLogout && await onLogout();
             return;
         }
