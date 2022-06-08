4920fead149fb4f17e238d2d88800a857c837e8f
Olivier Hoareau <oha@greenberets.io>
2022-06-08 10:19:26 +0200

    feat(array): add chunkize helper function

--- packages/array/src/index.ts
+++ packages/array/src/index.ts
@@ -1,1 +1,9 @@
 export const arrayize = v => Array.isArray(v) ? v : (undefined !== v ? [v] : []);
+export const chunkize = <T = string>(items: T[], size: number): (T[])[] => {
+    let i,j;
+    const chunks: (T[])[] = [];
+    for (i = 0,j = items.length; i < j; i += size) {
+        chunks.push(items.slice(i, i + size));
+    }
+    return chunks;
+};
