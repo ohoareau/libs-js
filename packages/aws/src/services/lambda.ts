498001f645c4043c7e57f6ec963560ca65481b3f
Olivier Hoareau <oha@greenberets.io>
2022-11-02 09:14:24 +0100

    feat(aws): add support for assume role in lambda invoke

--- packages/aws/src/services/lambda.ts
+++ packages/aws/src/services/lambda.ts
@@ -1,17 +1,19 @@
 import {InvokeError} from '@ohoareau/errors';
-const awslambda = new (require('aws-sdk/clients/lambda'));
+import Lambda from 'aws-sdk/clients/lambda';
+import STS from 'aws-sdk/clients/sts';
+const awsLambda = new (Lambda);
 
-const execute = async (arn, payload, options = {}) => {
+const execute = async (arn, payload, options: any = {}) => {
     options = {async: false, ...options};
     const logger = ((options ? options['logger'] : undefined) || console);
-    logger.log(`Invoking lambda '${arn}' with`, payload);
-    const result = await awslambda.invoke({
+    logger.log(`Invoking lambda '${arn}'${options?.assumeRoleArn ? ` on role '${options['assumeRoleArn']}'` : ''} with`, payload);
+    const result = await (await buildLambda(options['assumeRoleArn'])).invoke({
         FunctionName: arn,
         InvocationType: !!options['async'] ? 'Event' : 'RequestResponse',
         LogType: 'None',
-        Payload: JSON.stringify(payload),
+        Payload: 'string' === typeof payload ? payload : JSON.stringify(payload),
     }).promise();
-    const responsePayload = JSON.parse(result.Payload ? (result.Payload.toString ? result.Payload.toString() : result.Payload) : '{}');
+    const responsePayload = JSON.parse((result.Payload ? (result.Payload.toString ? result.Payload.toString() : result.Payload) : '{}') as any);
     if (result.FunctionError) throw new InvokeError(
         '?',
         '?',
@@ -21,6 +23,23 @@ const execute = async (arn, payload, options = {}) => {
     );
     logger.log(`Lambda '${arn}' ${!!options['async'] ? 'called asynchronously ' : ''}responded '${result.StatusCode}' with: `, responsePayload || result);
     return responsePayload;
+};
+
+
+const buildLambda = async (roleArn: string|undefined) => {
+    if(undefined === roleArn) return awsLambda;
+
+    const res = await new STS().assumeRole({
+        RoleArn: roleArn!,
+        RoleSessionName: 'session1',
+        DurationSeconds: 20*60,
+    }).promise();
+
+    return new Lambda({
+        accessKeyId: res.Credentials?.AccessKeyId,
+        secretAccessKey: res.Credentials?.SecretAccessKey,
+        sessionToken: res.Credentials?.SessionToken,
+    });
 };
 
 export const lambda = {execute}
