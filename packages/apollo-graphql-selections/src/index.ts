9ec3dee1d68d6feb57d45f6ae0bdc05317e31b32
Olivier Hoareau <oha@greenberets.io>
2022-12-19 00:44:18 +0100

    feat(apollo-graphql-selections): add arguments

--- packages/apollo-graphql-selections/src/index.ts
+++ packages/apollo-graphql-selections/src/index.ts
@@ -3,11 +3,25 @@ export function select(info: any) {
     return subSelect((info?.fieldNodes || [])[0]?.selectionSet?.selections || []);
 }
 
+function formatArgumentValue(val: { kind: string, value: any, block: boolean }) {
+    switch (val.kind) {
+        case 'StringValue': return String(val.value);
+        case 'IntValue': return parseInt(val.value);
+        default: return String(val.value);
+    }
+}
 export function subSelect(selections: any[]) {
     return selections.filter(x => x.kind === 'Field').reduce((acc, selection) => {
         acc.fields.push(selection.name.value);
         if (selection.selectionSet?.selections?.length) {
             acc.selections[selection.name.value] = subSelect(selection.selectionSet?.selections || []);
+        }
+        if (selection.arguments?.length) {
+            if (!acc.arguments) acc.arguments = {};
+            acc.arguments[selection.name.value] = selection.arguments.reduce((acc, arg) => {
+                acc[arg.name.value] = formatArgumentValue(arg.value);
+                return acc;
+            }, {} as any);
         }
         return acc;
     }, {fields: [], selections: {}});
