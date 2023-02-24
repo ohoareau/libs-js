5817e5ae1407a9413bed3c2089ce6e3b9cb4df36
flozonn <florian.breton1@icloud.com>
2023-02-24 13:27:53 +0100

    ADD messageGroupId
    In case if you are publishing to a fifo SNS topic the parameter messageGroupId is required.
    
    https://docs.aws.amazon.com/AWSSimpleQueueService/latest/SQSDeveloperGuide/using-messagegroupid-property.html

--- packages/aws/src/services/sns.ts
+++ packages/aws/src/services/sns.ts
@@ -1,8 +1,9 @@
 const awssns = new (require('aws-sdk/clients/sns'));
 
 export const sns = {
-    publish: async ({message, attributes, topic}) => awssns.publish({
+    publish: async ({message, attributes, topic, group }) => awssns.publish({
         Message: JSON.stringify(await message),
+        ...(group ? {MessageGroupId:group} : {}),
         MessageAttributes: Object.entries(attributes).reduce((acc, [k, v]) => {
             acc[k] = {DataType: 'String', StringValue: v};
             return acc;
