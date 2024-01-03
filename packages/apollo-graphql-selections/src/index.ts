4b9f2bf97d0f1a6125b44ceea6cee7704b63d9ab
Olivier Hoareau <oha@greenberets.io>
2024-01-03 01:12:04 +0100

    feat(apollo-graphql-selections): feat

--- packages/apollo-graphql-selections/src/index.ts
+++ packages/apollo-graphql-selections/src/index.ts
@@ -12,27 +12,13 @@ function formatArgumentValue(val: { kind: string, value: any, block: boolean }) 
 }
 export function subSelect(selections: any[]) {
     return selections.filter(x => x.kind === 'Field').reduce((acc, selection) => {
-        const alias = selection?.alias?.value;
-        const na = alias || selection.name.value;
-        if (alias) {
-            acc.selections[selection.name.value] = acc.selections[selection.name.value] || {};
-            acc.selections[selection.name.value].aliases = acc.selections[selection.name.value].aliases || [];
-            acc.selections[selection.name.value].aliases.push(selection?.alias?.value)
-        }
-        acc.fields.push(na);
+        acc.fields.push(selection.name.value);
         if (selection.selectionSet?.selections?.length) {
-            const old = acc.selections[na] || {};
-            const ss = subSelect(selection.selectionSet?.selections || []);
-            acc.selections[na] = {...old, ...ss, ...(old.aliases ? {aliases: [...old.aliases, ...(ss.aliases ? ss.aliases : [])]} : {}), ...(alias ? {aliasing: selection.name.value} : {})};
-        } else {
-            if (alias) {
-                const old = acc.selections[na] || {};
-                acc.selections[na] = {...old, aliasing: selection.name.value};
-            }
+            acc.selections[selection.name.value] = subSelect(selection.selectionSet?.selections || []);
         }
         if (selection.arguments?.length) {
             if (!acc.arguments) acc.arguments = {};
-            acc.arguments[na] = selection.arguments.reduce((acc, arg) => {
+            acc.arguments[selection.name.value] = selection.arguments.reduce((acc, arg) => {
                 acc[arg.name.value] = formatArgumentValue(arg.value);
                 return acc;
             }, {} as any);
