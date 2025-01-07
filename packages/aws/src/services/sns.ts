49a04da573f5b4eefb1449598bdba1bfba705862
Olivier Hoareau <oha@greenberets.io>
2025-01-07 22:57:34 +0100

    fix(aws): fix

--- packages/aws/src/services/sns.ts
+++ packages/aws/src/services/sns.ts
@@ -1,9 +1,19 @@
 const awssns = new (require('aws-sdk/clients/sns'));
 
+export type publish_order = {
+    message: any;
+    attributes: Record<string, any>;
+    topic: string;
+    group?: string;
+    deduplication?: string;
+    subject?: string;
+}
 export const sns = {
-    publish: async ({message, attributes, topic, group }: {message: any; attributes: Record<string, any>; topic: string; group?: string;}) => awssns.publish({
+    publish: async ({message, attributes, topic, group, deduplication, subject }: publish_order) => awssns.publish({
         Message: JSON.stringify(await message),
-        ...(group ? {MessageGroupId:group} : {}),
+        ...(group ? {MessageGroupId: group} : {}),
+        ...(deduplication ? {MessageDeduplicationId: deduplication} : {}),
+        ...(subject ? {Subject: subject} : {}),
         MessageAttributes: Object.entries(attributes).reduce((acc, [k, v]) => {
             acc[k] = {DataType: 'String', StringValue: v};
             return acc;
